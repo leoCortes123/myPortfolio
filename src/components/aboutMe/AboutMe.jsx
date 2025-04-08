0b8d29e22e57fcb743d966abfa0e505d7e81f320
DrLeoCortex <drleocortex@gmail.com>
2025-04-08 08:10:39 -0500

    Creation of section 2 of AboutMe and adjustments to the PXLDiv component to add background color to the container.

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -1,8 +1,8 @@
 // AboutMe.jsx
 import React from 'react';
 import PxlDiv from '../../utilities/PxlDiv.jsx';
+import Camera from './camera.jsx';
 import IconsContainer from './floatingIcons.jsx';
-import SliderImage2 from './sliderImage2.jsx';
 import styles from './styles/AboutMe.module.scss';
 
 
@@ -14,14 +14,15 @@ export default function AboutMe() {
       </div>
       <section className={styles.section1}>
         <div className={styles.presentation}>
-          <PxlDiv size={5} color="#000000">
-            <h1>¡HELLO!</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt, justo a facilisis gravida, velit neque varius odio, ac fermentum arcu odio et nisi. Fusce ut arcu ut odio auctor lacinia. Nam vel nisi sed purus tincidunt tincidunt. Curabitur non sapien eu odio varius euismod id et elit. Ut vitae mi eget augue pharetra feugiat. Sed in mi nec lorem venenatis tincidunt. Vestibulum et sapien sed libero vehicula congue ut vel eros.
-            </p>
-            <br />
-            <p>
-              Suspendisse potenti. Integer tincidunt magna ut justo volutpat, eget tristique nulla malesuada. Morbi ac quam sit amet justo interdum interdum. Nam tincidunt orci a nulla tincidunt, at pellentesque lacus interdum. Sed tincidunt ipsum at sem ultrices, eget dictum tortor posuere. Mauris fermentum pharetra augue, et feugiat neque vulputate sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-            </p>
+          <PxlDiv size={5} color="#000000" bgColor='RGB(228,228,200,1)'>
+            <div className={styles.textSection1}>
+              <h1>¡HELLO!</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt, justo a facilisis gravida, velit neque varius odio, ac fermentum arcu odio et nisi. Fusce ut arcu ut odio auctor lacinia. Nam vel nisi sed purus tincidunt tincidunt. Curabitur non sapien eu odio varius euismod id et elit. Ut vitae mi eget augue pharetra feugiat. Sed in mi nec lorem venenatis tincidunt. Vestibulum et sapien sed libero vehicula congue ut vel eros.
+              </p>
+              <p>
+                Suspendisse potenti. Integer tincidunt magna ut justo volutpat, eget tristique nulla malesuada. Morbi ac quam sit amet justo interdum interdum. Nam tincidunt orci a nulla tincidunt, at pellentesque lacus interdum. Sed tincidunt ipsum at sem ultrices, eget dictum tortor posuere. Mauris fermentum pharetra augue, et feugiat neque vulputate sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+              </p>
+            </div>
           </PxlDiv>
         </div>
         {/* <div className={styles.meditationContent}>
@@ -29,12 +30,16 @@ export default function AboutMe() {
         </div> */}
       </section>
       <section className={styles.section2}>
-        <SliderImage2 imageDirectory='../../assets/images/photograph' />
-        {/* <div className={styles.textSection}>
-          <h1>¡HELLO!</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt, justo a facilisis gravida, velit neque varius odio, ac fermentum arcu odio et nisi. Fusce ut arcu ut odio auctor lacinia. Nam vel nisi sed purus tincidunt tincidunt. Curabitur non sapien eu odio varius euismod id et elit. Ut vitae mi eget augue pharetra feugiat. Sed in mi nec lorem venenatis tincidunt. Vestibulum et sapien sed libero vehicula congue ut vel eros.
-          </p>
-        </div> */}
+        <PxlDiv size={5} color="#000000" bgColor='RGB(228,228,200,1)'>
+          <div className={styles.textSection2}>
+            <h1>¡HELLO!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tincidunt, justo a facilisis gravida, velit neque varius odio, ac fermentum arcu odio et nisi. Fusce ut arcu ut odio auctor lacinia. Nam vel nisi sed purus tincidunt tincidunt. Curabitur non sapien eu odio varius euismod id et elit. Ut vitae mi eget augue pharetra feugiat. Sed in mi nec lorem venenatis tincidunt. Vestibulum et sapien sed libero vehicula congue ut vel eros.
+            </p>
+          </div>
+          <div className={styles.Camera}>
+            <Camera />
+          </div>
+        </PxlDiv>
       </section>
     </div>
   );
