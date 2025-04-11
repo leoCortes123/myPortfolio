9297e4c5a3ed128fb2a126065bf63372cb428471
DrLeoCortex <drleocortex@gmail.com>
2025-04-11 16:06:28 -0500

    Revert "Changes to upload, share the project with Vercel, and change the page icon."
    This reverts commit b8234dc212770d964795a95c56560f594427d52f.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    sourcemap: true
-  },
   server: {
-    historyApiFallback: true,
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url === '/' || req.url.endsWith('.html')) {
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        }
+        next(); // Continuar sin modificar otros archivos
+      });
+    },
   },
 });
